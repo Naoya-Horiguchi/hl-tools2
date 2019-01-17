b664bae7691582688a322da5ed3cffc989aa2024
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2019-01-17 16:38:12 +0900

    cd before make
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>

--- scripts/codedeploy_validateservice.sh
+++ scripts/codedeploy_validateservice.sh
@@ -1,3 +1,13 @@
-make -C tests/v1.3.0/balance-transfer prepare
-make -C tests/v1.3.0/balance-transfer run_all
-bash scripts/save_output.sh $APPLICATION_NAME
+env
+cdir=$(pwd)
+testdir=deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/tests/v1.3.0/balance-transfer
+
+echo "cd $testdir" | tee -a /tmp/100
+cd $testdir 2>&1 | tee -a /tmp/100
+echo /opt/codedeploy-agent/$testdir 2>&1 | tee -a /tmp/100
+cd /opt/codedeploy-agent/$testdir 2>&1 | tee -a /tmp/100
+pwd | tee -a /tmp/100
+ls -ltra | tee -a /tmp/100
+echo "make prepare run_all" | tee -a /tmp/100
+# make prepare run_all
+# bash scripts/save_output.sh $APPLICATION_NAME
